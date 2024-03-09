8c9f29478f6e5a106ad9e95dc59cba20c1237881
Joakim Lindqvist <joakim@tech-mate.se>
2024-03-09 10:45:25 +0100

    feat: add initial Order and Address classes

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models;
+
+public class Order
+{
+	public Guid OrderId { get; set; }
+
+	public Guid CustomerId { get; set; }
+
+	public Address DeliveryAddress { get; set; }
+
+	public List<OrderLine> OrderLines { get; set; }
+}
