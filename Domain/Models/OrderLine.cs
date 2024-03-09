e538059e8ce7776904c3fbb893b91b95245f62e0
Joakim Lindqvist <joakim@tech-mate.se>
2024-03-09 10:42:10 +0100

    feat: add OrderLine

---
+++ Domain/Models/OrderLine.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models;
+
+public class OrderLine(Guid productId, int quantity)
+{
+	public readonly Guid ProductId = productId != Guid.Empty
+		? productId
+		: throw new ArgumentException("Id cannot be empty guid.", nameof(productId));
+
+	public readonly int Quantity = quantity > 0
+		? quantity
+		: throw new ArgumentOutOfRangeException(nameof(quantity), "Value must be greater than zero.");
+}
