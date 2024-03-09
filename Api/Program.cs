1a25f4e19845767545619fd08dabcaf613adc7d1
Joakim Lindqvist <joakim@tech-mate.se>
2024-03-09 13:18:31 +0100

    feat: enable XML comments for Swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,7 @@ using Api.ErrorHandling;
 using Application.ExternalInterfaces;
 using Application.Handlers;
 using Infrastructure.OrderPersistance.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,12 @@ builder.Services.AddSingleton<IOrderRepository, OrderRepositoryScaffold>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
